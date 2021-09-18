663e47687393b5dc5331850b707f243b211ec83a
Zainul Abid <48980248+zainulhibath@users.noreply.github.com>
2021-09-18 19:24:43 +0530

    Create config.py

---
+++ publicleechgroup/config.py
@@ -0,0 +1,7 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+  TG_BOT_TOKEN = "2009520571:AAG2nrvQzumURUsJPMe9pANbZnri2-dlIx4"
+  APP_ID = "3881325"
+  API_HASH = "1ebf12e67222683abe1c01f0209b79df"
+  AUTH_CHANNEL = -1001588199268
